9b8aa541e4d56dd5fec3d09cb74bf778e6489497
Vinay <vinay@obvious.in>
2021-03-04 17:25:57 +0530

    Identify generated Room DAO classes

--- src/main/kotlin/org/simple/rmg/App.kt
+++ src/main/kotlin/org/simple/rmg/App.kt
@@ -1,5 +1,7 @@
 package org.simple.rmg
 
+import com.github.javaparser.StaticJavaParser
+import com.github.javaparser.ast.CompilationUnit
 import java.io.File
 import java.nio.file.Paths
 
@@ -26,10 +28,27 @@ class App {
 			.walkTopDown()
 			.filter { it.isFile }
 			.filter(File::isJavaSourceFile)
+			.map(File::readText)
+			.filter(String::containsRoomImport)
+			.map { StaticJavaParser.parse(it) }
+			.filter(CompilationUnit::isGeneratedRoomDao)
+			.map { it.className() }
 			.toList()
 
-		logger.info("All generated Java sources: ${generatedJavaSourceFiles.joinToString("\n")}")
+		logger.info("All generated Java sources:\n${generatedJavaSourceFiles.joinToString("\n")}")
 	}
 }
 
 private fun File.isJavaSourceFile(): Boolean = name.endsWith(".java")
+
+private fun String.containsRoomImport(): Boolean {
+	return contains("import androidx.room.RoomDatabase;")
+}
+
+private fun CompilationUnit.className(): String {
+	return types.first().nameAsString
+}
+
+private fun CompilationUnit.isGeneratedRoomDao(): Boolean {
+	return className().contains("_Impl")
+}
