e8ac2549c83ec27663bef029049dd88013bde827
Vinay <vinay@obvious.in>
2021-03-04 15:50:10 +0530

    Find path to generated source directory

--- src/main/kotlin/org/simple/rmg/App.kt
+++ src/main/kotlin/org/simple/rmg/App.kt
@@ -1,14 +1,24 @@
 package org.simple.rmg
 
+import java.nio.file.Paths
+
 fun main(args: Array<String>) {
-	App().run(args[0])
+	App().run(args[0], args[1])
 }
 
 class App {
 
 	private val logger = logger<App>()
 
-	fun run(path: String) {
-		logger.info("Generate metadata for project: $path")
+	fun run(path: String, module: String) {
+		logger.info("Generate metadata for project: $path/$module")
+
+		val (moduleDirectoryName, sourceSet) = module.split(':')
+		val moduleRootDirectory = Paths.get(path, moduleDirectoryName)
+
+		val moduleGeneratedSourcesDirectory =
+			moduleRootDirectory.resolve(Paths.get("build", "generated", "source", "kapt", "qaDebug"))
+
+		logger.info("Build directory: $moduleGeneratedSourcesDirectory")
 	}
 }
