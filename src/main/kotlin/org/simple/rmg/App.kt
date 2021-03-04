5b318e9c05f50eb18aba8dcf4eb15b318addf484
Vinay <vinay@obvious.in>
2021-03-04 17:54:19 +0530

    Extract all Room SQL method metadata

--- src/main/kotlin/org/simple/rmg/App.kt
+++ src/main/kotlin/org/simple/rmg/App.kt
@@ -2,6 +2,7 @@ package org.simple.rmg
 
 import com.github.javaparser.StaticJavaParser
 import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.body.MethodDeclaration
 import java.io.File
 import java.nio.file.Paths
 
@@ -24,7 +25,7 @@ class App {
 
 		logger.info("Build directory: $moduleGeneratedSourcesDirectory")
 
-		val generatedJavaSourceFiles = moduleGeneratedSourcesDirectory.toFile()
+		val generatedRoomDaoAsts = moduleGeneratedSourcesDirectory.toFile()
 			.walkTopDown()
 			.filter { it.isFile }
 			.filter(File::isJavaSourceFile)
@@ -32,10 +33,24 @@ class App {
 			.filter(String::containsRoomImport)
 			.map { StaticJavaParser.parse(it) }
 			.filter(CompilationUnit::isGeneratedRoomDao)
-			.map { it.className() }
 			.toList()
 
-		logger.info("All generated Java sources:\n${generatedJavaSourceFiles.joinToString("\n")}")
+		logger.info("All generated Java sources:\n${generatedRoomDaoAsts.joinToString(separator = "\n", transform = CompilationUnit::className)}")
+
+		val methodInformations = generatedRoomDaoAsts
+			.flatMap { compilationUnit ->
+				compilationUnit
+					.methods()
+					.map { methodDeclaration -> compilationUnit to methodDeclaration }
+			}
+			.map { (compilationUnit, methodDeclaration) ->
+				val methodRange = methodDeclaration.range
+				"${compilationUnit.className()}.${methodDeclaration.nameAsString} [${methodRange.get()}]"
+			}
+			.toList()
+			.joinToString("\n")
+
+		logger.info("Methods:\n$methodInformations")
 	}
 }
 
@@ -51,4 +66,8 @@ private fun CompilationUnit.className(): String {
 
 private fun CompilationUnit.isGeneratedRoomDao(): Boolean {
 	return className().contains("_Impl")
+}
+
+private fun CompilationUnit.methods(): List<MethodDeclaration> {
+	return types.first().methods
 }
