10992236f95a946ac5dcae61fafa5aebf7489ca3
Vinay <vinay@obvious.in>
2021-03-30 17:10:19 +0530

    Add canary test to exercise the test harness

---
+++ src/test/kotlin/org/simple/rmg/CanaryTest.kt
@@ -0,0 +1,12 @@
+package org.simple.rmg
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class CanaryTest {
+
+	@Test
+	fun `test harness is set up correctly`() {
+		assertThat(true).isTrue()
+	}
+}
