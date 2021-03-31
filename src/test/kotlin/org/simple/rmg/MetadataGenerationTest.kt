f471b94ab9019e22e3c45a9f2595b156a55052fd
Vinay <vinay@obvious.in>
2021-03-31 11:19:13 +0530

    Add integration test to verify metadata generation for multiple DAO implementations

--- src/test/kotlin/org/simple/rmg/MetadataGenerationTest.kt
+++ src/test/kotlin/org/simple/rmg/MetadataGenerationTest.kt
@@ -33,6 +33,37 @@ class MetadataGenerationTest {
 		assertThat(roomMetadata).isEqualTo(expectedMetadata)
 	}
 
+	@Test
+	fun `generating the metadata for multiple generated dao should generate the metadata for all`() {
+		// given
+		val userDaoImplementation = readResource("test_data/UserRoomDao_Impl.java")
+		val overdueAppointmentDaoImplementation = readResource("test_data/OverdueAppointmentRoomDao_Impl.java")
+
+		// when
+		val roomMetadata = app.generateRoomMetadataForSources(listOf(userDaoImplementation, overdueAppointmentDaoImplementation))
+
+		// then
+		val expectedMetadata = """
+			|UserRoomDao_Impl,createOrUpdate,184,194
+			|UserRoomDao_Impl,deleteUser,196,206
+			|UserRoomDao_Impl,updateLoggedInStatusForUser,208,237
+			|UserRoomDao_Impl,setCurrentFacility,239,260
+			|UserRoomDao_Impl,user,262,346
+			|UserRoomDao_Impl,userImmediate,348,424
+			|UserRoomDao_Impl,userCount,426,461
+			|UserRoomDao_Impl,currentFacility,463,598
+			|UserRoomDao_Impl,currentFacilityImmediate,600,727
+			|UserRoomDao_Impl,currentFacilityUuid,729,749
+			|UserRoomDao_Impl,userAndFacilityDetails,751,786
+			|OverdueAppointmentRoomDao_Impl,overdueAtFacility,65,319
+			|OverdueAppointmentRoomDao_Impl,overdueAtFacilityDataSource,321,570
+			|OverdueAppointmentRoomDao_Impl,overdueAtFacilityCount,572,645
+			|OverdueAppointmentRoomDao_Impl,latestForPatient,647,884
+		""".trimMargin()
+
+		assertThat(roomMetadata).isEqualTo(expectedMetadata)
+	}
+
 	private fun readResource(resourcePath: String): String {
 		return javaClass.classLoader.getResourceAsStream(resourcePath)!!.reader().readText()
 	}
