a7f8087f873027e56c64da9385f625120a1712a0
Vinay <vinay@obvious.in>
2021-06-10 10:18:34 +0530

    Remove code related to metadata generation

--- src/main/kotlin/org/simple/rmg/MetadataGenerationResult.kt
+++
@@ -1,7 +0,0 @@
-package org.simple.rmg
-
-sealed class MetadataGenerationResult
-
-data class Succeeded(val metadata: String) : MetadataGenerationResult()
-
-data class OverloadedMethodsFound(val methods: Map<String, Set<String>>) : MetadataGenerationResult()
--- src/main/kotlin/org/simple/rmg/RoomMetadataGenerator.kt
+++ src/main/kotlin/org/simple/rmg/RoomMetadataGenerator.kt
@@ -18,7 +18,6 @@ import java.io.File
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-//	RoomMetadataGenerator().run(args[0], args[1], args[2])
 	RoomMetadataGenerator().readMeasureMethod(args[0], args[1])
 }
 
@@ -27,89 +26,6 @@ private val rxJavaTypes = setOf("Single", "Completable", "Observable", "Flowable
 class RoomMetadataGenerator {
 
 	private val logger = logger<RoomMetadataGenerator>()
-
-	fun run(projectPath: String, sourceSet: String, outputCsvPath: String) {
-		logger.info("Generate metadata for project: $projectPath/$sourceSet")
-
-		val moduleGeneratedSourcesDirectory = Paths.get(projectPath, "build", "generated", "source", "kapt", sourceSet)
-		logger.info("Build directory: $moduleGeneratedSourcesDirectory")
-
-		val generatedRoomDaoImplementations = moduleGeneratedSourcesDirectory.toFile()
-			.walkTopDown()
-			.filter { it.isFile }
-			.filter(File::isJavaSourceFile)
-			.map { it.readText() }
-			.filter(String::containsRoomImport)
-			.toList()
-
-		when (val result = generateRoomMetadataForSources(generatedRoomDaoImplementations)) {
-			is OverloadedMethodsFound -> reportOverloadedMethodsFound(result.methods)
-			is Succeeded -> {
-				val outputCsvFile = Paths.get(outputCsvPath).toFile()
-				outputCsvFile.parentFile.mkdirs()
-				writeMetadataToOutputFile(result.metadata, outputCsvFile.path)
-			}
-		}
-	}
-
-	private fun writeMetadataToOutputFile(csv: String, outputCsvPath: String) {
-		logger.info("----- BEGIN DB METADATA -----\n\n${csv}\n\n----- END DB METADATA -----")
-
-		logger.info("Write generated metadata to $outputCsvPath")
-
-		with(File(outputCsvPath)) {
-			writeText(csv)
-		}
-	}
-
-	private fun reportOverloadedMethodsFound(overloadedMethods: Map<String, Set<String>>) {
-		val errorMessage = overloadedMethods
-			.map { (className, methodNames) ->
-				val joinedMethodNames = methodNames.joinToString()
-				"$className : [$joinedMethodNames]"
-			}
-			.joinToString(separator = "\n", prefix = "Found overloaded methods:\n\n")
-
-		logger.error(errorMessage)
-
-		throw RuntimeException(errorMessage)
-	}
-
-	fun generateRoomMetadataForSources(daoImplementations: List<String>): MetadataGenerationResult {
-		val generatedRoomDaoAsts = daoImplementations
-			.map { StaticJavaParser.parse(it) }
-			.filter(CompilationUnit::isGeneratedRoomDao)
-
-		val methodInfos = generatedRoomDaoAsts
-			.flatMap { compilationUnit ->
-				compilationUnit
-					.methods()
-					.map { methodDeclaration -> MethodInfo(compilationUnit, methodDeclaration) }
-			}
-
-		val foundMethodOverloads = findMethodsWithOverloads(methodInfos)
-
-		return when {
-			foundMethodOverloads.isNotEmpty() -> OverloadedMethodsFound(foundMethodOverloads)
-			else -> Succeeded(methodInfos.joinToString("\n", transform = ::generateMethodCsvLine))
-		}
-	}
-
-	private fun findMethodsWithOverloads(infos: List<MethodInfo>): Map<String, Set<String>> {
-		return infos
-			.groupBy(
-				keySelector = { it.className },
-				valueTransform = { it.methodName }
-			)
-			.mapValues { (_, methods) -> methods.removeUniqueElements() }
-			.filterValues(Set<String>::isNotEmpty)
-	}
-
-	private fun generateMethodCsvLine(
-		methodInfo: MethodInfo
-	): String {
-		return "${methodInfo.className},${methodInfo.methodName},${methodInfo.methodLineNumbers.first},${methodInfo.methodLineNumbers.last}"
-	}
 
 	fun readMeasureMethod(
 		projectPath: String,
@@ -283,32 +199,9 @@ private fun CompilationUnit.methods(): List<MethodDeclaration> {
 	return types.first().methods
 }
 
-private data class MethodInfo(
-	val className: String,
-	val methodName: String,
-	val methodLineNumbers: IntRange
-) {
-
-	constructor(compilationUnit: CompilationUnit, methodDeclaration: MethodDeclaration) : this(
-		className = compilationUnit.className(),
-		methodName = methodDeclaration.nameAsString,
-		methodLineNumbers = methodDeclaration.range.get().begin.line..methodDeclaration.range.get().end.line
-	)
-}
-
 private data class DaoMetadata(
 	val file: File,
 	val ast: CompilationUnit
 ) {
 	fun replaceAst(newAst: CompilationUnit) = copy(ast = newAst)
-}
-
-private fun List<String>.removeUniqueElements(): Set<String> {
-	return associateBy(
-		keySelector = { it },
-		valueTransform = { methodName -> this.count { it == methodName } }
-	)
-		.filterValues { countOfMethodNames -> countOfMethodNames > 1 }
-		.keys
-		.toSet()
 }
